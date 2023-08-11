87b0454195df45189fc7278aeda4279c2cce20ea
Nathan Gauër <brioche@google.com>
2023-08-11 14:53:53 +0200

    fixup! [SPIRV-V] Add SPIR-V logical triple to llc

--- llvm/lib/Target/SPIRV/SPIRVAsmPrinter.cpp
+++ llvm/lib/Target/SPIRV/SPIRVAsmPrinter.cpp
@@ -476,7 +476,6 @@ void SPIRVAsmPrinter::outputExecutionMode(const Module &M) {
       Inst.addOperand(MCOperand::createImm(TypeCode));
       outputMCInst(Inst);
     }
-
     if (ST->isOpenCLEnv() && !M.getNamedMetadata("spirv.ExecutionMode") &&
         !M.getNamedMetadata("opencl.enable.FP_CONTRACT")) {
       MCInst Inst;
