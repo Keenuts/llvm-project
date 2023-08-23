abce33f420f718c07924c9331a36b0da1a9d3fdb
Nathan Gauër <brioche@google.com>
2023-08-23 17:51:35 +0200

    fixup! [SPIR-V] Add SPIR-V logical triple.
    get pointer size from data layout

--- llvm/lib/Target/SPIRV/SPIRVTargetMachine.cpp
+++ llvm/lib/Target/SPIRV/SPIRVTargetMachine.cpp
@@ -46,6 +46,10 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeSPIRVTarget() {
 
 static std::string computeDataLayout(const Triple &TT) {
   const auto Arch = TT.getArch();
+  // TODO: this probably needs to be revisited:
+  //  AFAIU Logical SPIR-V has no pointer size. So falling-back on ID size.
+  //  Addressing mode can change how some pointers are handled
+  //  (PhysicalStorageBuffer64).
   if (Arch == Triple::spirv32 || Arch == Triple::spirv)
     return "e-p:32:32-i64:64-v16:16-v24:32-v32:32-v48:64-"
            "v96:128-v192:256-v256:256-v512:512-v1024:1024";
